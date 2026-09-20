c150e93ab0ca41a85ea98ad502b7952a92e864d5
vanshmunjal_21 <139635345+vansh211@users.noreply.github.com>
2026-09-20 09:05:50 +0530

    Time: 1272 ms (5.01%) | Memory: 84.9 MB (19.30%) - LeetSync

--- 1206-corporate-flight-bookings/corporate-flight-bookings.java
+++ 1206-corporate-flight-bookings/corporate-flight-bookings.java
@@ -6,8 +6,6 @@ class Solution {
             for(int j = b[0] - 1; j <= b[1] - 1; j++){
                 ans[j] += b[2];
             }
-
-            System.out.println(ans[b[0] - 1] + " " + ans[b[1] - 1]);
         }
 
         return ans;
