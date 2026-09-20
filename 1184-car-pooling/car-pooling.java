e27b46cf6b6104b8875fcb49f62d946b0b4204d2
vanshmunjal_21 <139635345+vansh211@users.noreply.github.com>
2026-09-20 09:00:21 +0530

    Time: 1 ms (100.00%) | Memory: 46.4 MB (24.84%) - LeetSync

--- 1184-car-pooling/car-pooling.java
+++ 1184-car-pooling/car-pooling.java
@@ -1,11 +1,21 @@
 class Solution {
-   public boolean carPooling(int[][] trips, int capacity) {    
-  int stops[] = new int[1001]; 
-    for (int t[] : trips) {
-      stops[t[1]] += t[0];
-      stops[t[2]] -= t[0];
-  }
-  for (int i = 0; capacity >= 0 && i < 1001; ++i) capacity -= stops[i];
-  return capacity >= 0;
-}
+    public boolean carPooling(int[][] trips, int capacity) {
+        int lastDestination = 0;
+        for(int[] trip : trips) lastDestination = Math.max(lastDestination, trip[2]);
+        int[] diffArray = new int[lastDestination+1];
+        for(int[] trip : trips){
+            int value = trip[0];
+            int start = trip[1];
+            int end = trip[2];
+            diffArray[start] += value;
+            diffArray[end] -= value;
+        }
+        int temp = diffArray[0];
+        if(temp > capacity) return false;
+        for(int i=1;i<lastDestination;i++){
+            temp += diffArray[i];
+            if(temp > capacity) return false;
+        }
+    return true;
+    }
 }
