8853c95b73d533c66b6a96e777a080224557e2da
vanshmunjal_21 <139635345+vansh211@users.noreply.github.com>
2026-09-20 09:12:05 +0530

    Time: 3 ms (99.85%) | Memory: 49 MB (90.66%) - LeetSync

---
+++ 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.java
@@ -0,0 +1,12 @@
+class Solution {
+        public int subarraysDivByK(int[] A, int K) {
+        int[] count = new int[K];
+        count[0] = 1;
+        int prefix = 0, res = 0;
+        for (int a : A) {
+            prefix = (prefix + a % K + K) % K;
+            res += count[prefix]++;
+        }
+        return res;
+    }
+}
